9c9cdffb43dc34fd7f34cb164e01683c5726ff25
Reinpeter Ondeyo Momanyi <reinp.mom@gmail.com>
2026-09-16 15:41:08 +0300

    Update SQL ETL Scripts

--- SQL ETL Scripts/3.create_schemas.sql
+++ SQL ETL Scripts/3.create_schemas.sql
@@ -1,6 +1,6 @@
 ---Creating Schemas
 
-CREATE SCHEMA IF NOT EXISTS omop_training.nids_cdm;
-CREATE SCHEMA IF NOT EXISTS omop_training.nids_results;
-CREATE SCHEMA IF NOT EXISTS omop_training.vocabulary;
+CREATE SCHEMA IF NOT EXISTS "omop_training.nids_cdm";
+CREATE SCHEMA IF NOT EXISTS "omop_training.nids_results";
+CREATE SCHEMA IF NOT EXISTS "omop_training.vocabulary";
 
