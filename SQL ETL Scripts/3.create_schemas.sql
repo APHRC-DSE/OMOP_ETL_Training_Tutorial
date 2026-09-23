774f2d2f2ed28879465d585745f9b94f13365185
kochollas <kochollamikes@gmail.com>
2026-09-23 12:18:53 +0300

    updated

--- SQL ETL Scripts/3.create_schemas.sql
+++
@@ -1,8 +0,0 @@
----Creating Schemas
-
-/*Select the omop_training database in pgAdmin, right-click, and open a query tool*/
-
-CREATE SCHEMA IF NOT EXISTS nids_cdm;
-CREATE SCHEMA IF NOT EXISTS nids_results;
-CREATE SCHEMA IF NOT EXISTS vocabulary;
-
